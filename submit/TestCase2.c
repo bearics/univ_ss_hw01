c9f68507f6eff17410e3008b454a775f8c1600cb
bearics <bearics@gmail.com>
2017-12-06 22:50:17 +0900

    Fin to submit

--- submit/TestCase2.c
+++ submit/TestCase2.c
@@ -4,10 +4,9 @@ void* Tc2ThreadProc(void* param)
 {
 	thread_t tid = 0;
 	int count = 0;
-	int i;
 	int* retVal;
 	tid = thread_self();
-	for(int i=0;i<3;i++){
+	for(int i=0;i<10;i++){
 		sleep(2);
 		printf("Tc1ThreadProc: my thread id (%d), arg is (%d)\n", (int)tid, *((int*)param));
 		count++;
@@ -29,7 +28,6 @@ TestCase2(void)
 	thread_t tid[TOTAL_THREAD_NUM];
 	int result[TOTAL_THREAD_NUM];
 
-	int i = 0;
 	thread_create(&tid[0], NULL, (void*)Tc2ThreadProc,(void*) &i1);
 	thread_create(&tid[1], NULL, (void*)Tc2ThreadProc,(void*) &i2);
 	thread_create(&tid[2], NULL, (void*)Tc2ThreadProc,(void*) &i3);
@@ -37,7 +35,7 @@ TestCase2(void)
 	thread_create(&tid[4], NULL, (void*)Tc2ThreadProc,(void*) &i5);
 
 
-	for(i=0;i<TOTAL_THREAD_NUM;i++)
+	for(int i=0;i<TOTAL_THREAD_NUM;i++)
 	{
 		int* retVal;
 		thread_join(tid[i],(void **)&retVal);
